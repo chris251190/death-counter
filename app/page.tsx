890d8a5999b7a847f4239e4662415b4ab2b707ea
chris251190 <christian_lehr.1@gmx.de>
2023-12-22 12:41:26 +0100

    Refactor layout and add pig image link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <Image src="/pig.jpg" alt="Description of the image" width={500} height={300} />
+      <Link href="/pigs">
+        <Image src="/pig.jpg" alt="Description of the image" width={500} height={300} />
+      </Link>
       Foto von <a href="https://unsplash.com/de/@bhris1017?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Christopher Carson</a> auf <a href="https://unsplash.com/de/fotos/weisses-ferkel-kaut-heu-i4XLJmlYit4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
     </main>
   )
--- pages/pigs.tsx
+++ pages/pigs.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Pigs: React.FC = () => {
+  const pigsPerYear = 1400092105;
+  const secondsPerYear = 60 * 60 * 24 * 365;
+
+  const calculatePigsPerSecond = () => {
+    return pigsPerYear / secondsPerYear;
+  };
+
   return (
     <div>
-      <h1>New Page</h1>
-      <p>This is a new page under the /pigs route.</p>
+      <h1>Pigs</h1>
+      <p>{pigsPerYear} Pigs are killed per year worldwide. That means {calculatePigsPerSecond().toFixed(2)} pig deaths per second.</p>
     </div>
   );
 };
