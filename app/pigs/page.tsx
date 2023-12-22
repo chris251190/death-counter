cdd3b7a62c6f640f0762a824e027aca294a08043
chris251190 <christian_lehr.1@gmx.de>
2023-12-22 13:45:46 +0100

    Refactor pig page component to track and display pigs killed this year

--- app/pigs/page.tsx
+++ app/pigs/page.tsx
@@ -4,37 +4,45 @@ import Image from 'next/image'
 import Link from 'next/link';
 
 const Pigs: React.FC = () => {
-  const pigsPerYear = 1400092105;
-  const secondsPerYear = 60 * 60 * 24 * 365;
+    const pigsPerYear = 1400092105;
+    const secondsPerYear = 60 * 60 * 24 * 365;
 
-  const calculatePigsPerSecond = () => {
-    return pigsPerYear / secondsPerYear;
-  };
+    const calculatePigsPerSecond = () => {
+        return pigsPerYear / secondsPerYear;
+    };
 
-  const [killedPigs, setKilledPigs] = useState(0);
+    const [killedPigs, setKilledPigs] = useState(0);
+    const [killedPigsThisYear, setKilledPigsThisYear] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setKilledPigs((prevKilledPigs) => prevKilledPigs + calculatePigsPerSecond());
-    }, 1000);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const startOfYear = new Date(now.getFullYear(), 0, 1);
+            const secondsSoFar = Math.floor((now.getTime() - startOfYear.getTime()) / 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+            setKilledPigsThisYear(Math.floor(calculatePigsPerSecond() * secondsSoFar));
+            setKilledPigs((prevKilledPigs) => prevKilledPigs + calculatePigsPerSecond());
+        }, 1000);
 
-  return (
-    <div>
-      <h1>Pigs</h1>
-      <Image src="/pig.jpg" alt="Description of the image" width={500} height={300} />
-      <p><b>{pigsPerYear.toLocaleString()} Pigs</b> are killed per year worldwide. That means roughly <b>{Math.floor(calculatePigsPerSecond())} deaths per second.</b></p>
+        return () => clearInterval(interval);
+    }, []);
 
-      <p><b>{Math.floor(killedPigs)} pigs have been killed since you opened this page.</b></p>
+    return (
+        <div>
+            <h1>Pigs</h1>
+            <Image src="/pig.jpg" alt="Description of the image" width={500} height={300} />
+            <p><b>{pigsPerYear.toLocaleString()} Pigs</b> are killed per year worldwide. That means roughly <b>{Math.floor(calculatePigsPerSecond())} deaths per second.</b></p>
 
-      <p>The data used here are from: <a href="https://viva.org.uk/animals/number-animals-killed/">https://viva.org.uk/animals/number-animals-killed/</a></p>
+            <p><b>{Math.floor(killedPigs)} pigs have been killed since you opened this page.</b></p>
 
-      <Link href="/">Go Back</Link>
-      
-    </div>
-  );
+            <p><b>{killedPigsThisYear.toLocaleString()} pigs have been killed this year so far.</b></p>
+
+            <p>The data used here are from: <a href="https://viva.org.uk/animals/number-animals-killed/">https://viva.org.uk/animals/number-animals-killed/</a></p>
+
+            <Link href="/">Go Back</Link>
+
+        </div>
+    );
 };
 
 export default Pigs;
