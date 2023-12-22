615c42f4e5d17ee372ff5d6036213dc32352e1bd
chris251190 <christian_lehr.1@gmx.de>
2023-12-22 14:07:00 +0100

    Refactor image layout in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,35 @@ export default function Home() {
     <main>
       <h1>Animal Deaths</h1>
 
-      <div>
-      Photo from <a href="https://unsplash.com/de/@bhris1017?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Christopher Carson</a> on <a href="https://unsplash.com/de/fotos/weisses-ferkel-kaut-heu-i4XLJmlYit4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-      <Link href="/pigs">
-        <Image src="/pig.jpg" alt="Description of the image" width={500} height={300} />
-      </Link>
+      <div className="flex flex-wrap">
+        <div className="w-1/3 p-2">
+          <div>
+            Photo from <a href="https://unsplash.com/de/@bhris1017?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Christopher Carson</a> on <a href="https://unsplash.com/de/fotos/weisses-ferkel-kaut-heu-i4XLJmlYit4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            <Link href="/pigs">
+              <Image src="/pig.jpg" alt="Description of the image" width={500} height={300} />
+            </Link>
+          </div>
+        </div>
+
+        <div className="w-1/3 p-2">
+          <div>
+            Photo from <a href="https://unsplash.com/de/@dave_george?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">David George</a> on <a href="https://unsplash.com/de/fotos/braune-kuh-auf-grunem-grasfeld-tagsuber-o41CI8825qU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            <Link href="/cattle">
+              <Image src="/cattle.jpg" alt="Description of the image" width={500} height={300} />
+            </Link>
+          </div>
+        </div>
+
+        <div className="w-1/3 p-2">
+          <div>
+            Photo from <a href="https://unsplash.com/de/@cobybriant?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Brian David</a> on <a href="https://unsplash.com/de/fotos/weisse-ente-mit-entenkuken-auf-braunem-nest-gi4p27XKVY8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            <Link href="/chicken">
+              <Image src="/chicken.jpg" alt="Description of the image" width={500} height={300} />
+            </Link>
+          </div>
+        </div>
       </div>
 
-      <div>
-      Photo from <a href="https://unsplash.com/de/@dave_george?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">David George</a> on <a href="https://unsplash.com/de/fotos/braune-kuh-auf-grunem-grasfeld-tagsuber-o41CI8825qU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-      <Link href="/cattle">
-        <Image src="/cattle.jpg" alt="Description of the image" width={500} height={300} />
-      </Link>
-  
-      </div>
-
-      <div>
-      Photo from <a href="https://unsplash.com/de/@cobybriant?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Brian David</a> on <a href="https://unsplash.com/de/fotos/weisse-ente-mit-entenkuken-auf-braunem-nest-gi4p27XKVY8?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-        <Link href="/chicken">
-        <Image src="/chicken.jpg" alt="Description of the image" width={500} height={300} />
-      </Link>
-  
-      </div>
-     
     </main>
   )
 }
