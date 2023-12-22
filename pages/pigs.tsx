2a67d2c085d44b1984858cc17ea8944f7f049608
chris251190 <christian_lehr.1@gmx.de>
2023-12-22 12:51:12 +0100

    Add live update of killed pigs count

--- pages/pigs.tsx
+++ pages/pigs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image'
 
 const Pigs: React.FC = () => {
@@ -9,13 +9,24 @@ const Pigs: React.FC = () => {
     return pigsPerYear / secondsPerYear;
   };
 
+  const [killedPigs, setKilledPigs] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setKilledPigs((prevKilledPigs) => prevKilledPigs + calculatePigsPerSecond());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       <h1>Pigs</h1>
       <Image src="/pig.jpg" alt="Description of the image" width={500} height={300} />
+      <p>{pigsPerYear.toLocaleString()} Pigs are killed per year worldwide. That means roughly {Math.floor(calculatePigsPerSecond())} pig deaths per second.</p>
 
-      <p>{pigsPerYear.toLocaleString()} Pigs are killed per year worldwide. That means {calculatePigsPerSecond().toFixed(2)} pig deaths per second.</p>
-    </div>
+          <p style={{ fontWeight: 'bold' }}>{Math.floor(killedPigs)} pigs have been killed since you opened this page.</p>
+        </div>
   );
 };
 
