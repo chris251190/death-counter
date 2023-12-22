5b0f020b96afaf8ad9083545cd2b8a8b23a166e3
chris251190 <christian_lehr.1@gmx.de>
2023-12-22 11:56:24 +0100

    add pigs page

---
+++ pages/pigs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pigs: React.FC = () => {
+  return (
+    <div>
+      <h1>New Page</h1>
+      <p>This is a new page under the /pigs route.</p>
+    </div>
+  );
+};
+
+export default Pigs;
